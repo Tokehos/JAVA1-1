442fda0c07bb2c7cf78f765d839e4d09e8a0ba1b
Tokehos <88163971+Tokehos@users.noreply.github.com>
2021-09-09 13:27:40 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int transfer = 500000000;
+        int totalBalance = balance + transfer;
+        System.out.println(totalBalance);
+    }
+}
